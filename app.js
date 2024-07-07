561ff26c9afd07ed538babf9bb7ebff2fef66f94
Abhav Goel <goelabhav2002@gmail.com>
2024-07-08 02:32:40 +0530

    final touches

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const connectToMongoDb = require("./dbConnection");
+const { requestLogger } = require("./middlewares/requestLogger");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
+require("dotenv").config();
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//middlewares
+app.use(requestLogger("serverlog.txt"));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+//view engine setup
+app.set("view engine" , "ejs");
+app.set("views", path.resolve("./views"));
+
+connectToMongoDb(process.env.MONGO_URL).then(() => {
+    console.log("MongoDB connected");
+});
+
+app.get("/" , async (req,res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home" , {
+        user : req.user,
+        blogs : allBlogs
+    });
+});
+
+app.use("/user",userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+})
+
